b482ef3b14bde550d245177e86fc41768e45438b
ErdniSamkhaev <samhaev.e@yandex.ru>
2025-02-03 22:48:04 +0300

    first commit

---
+++ task1/VueVite/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Hello Vue and Vite</h1>
+  <Greeting :name="userName || 'Guest'" />
+  <button @click="increment">Click me</button>
+  <p>Count: {{ count }}</p>
+  <form action="#">
+    <input
+      type="text"
+      name="name"
+      id="name"
+      v-model="userName"
+      placeholder="Enter your name"
+    />
+  </form>
+  <p v-if="userName">Username: {{ userName }}</p>
+</template>
+
+<script setup>
+// Импорты
+import { ref } from "vue";
+import Greeting from "./components/Greeting.vue";
+
+// Реактивные переменные
+const count = ref(0);
+const userName = ref("");
+// Фнукция инкремент
+function increment() {
+  count.value++;
+}
+</script>
+
+<style scoped></style>
---
+++ task1/VueVite/src/components/Greeting.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>Hello, {{ name }}</h2>
+</template>
+
+<script setup>
+defineProps(["name"]);
+</script>
