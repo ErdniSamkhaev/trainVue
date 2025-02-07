dc09ce748ba224d8b5a33f41aee8af272bb7359e
ErdniSamkhaev <samhaev.e@yandex.ru>
2025-02-07 22:53:21 +0300

    doc Greeting.vue

--- task1/VueVite/src/components/Greeting.vue
+++ task1/VueVite/src/components/Greeting.vue
@@ -1,9 +1,22 @@
 <template>
+  <!-- Тег h2 и интерполяция {{ name }} -->
+  <!-- name - реактивная переменная из props -->
   <h2>Hello, {{ name }}</h2>
 </template>
 
 <script setup lang="ts">
+// script setup composition API
+// defineProps макрос Vue
+// <> - типизация () -вызов функции defineProps
+// {} - для пропсов нужно писать объектную структуру
 defineProps<{
+  /**
+   * Имя
+   * @example
+   * name: 'John'
+   * Обязательность пропса
+   * @required
+   */
   name: string;
 }>();
 </script>
